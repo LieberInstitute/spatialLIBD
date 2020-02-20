1c84161b312da4369e1f4830c55ff39c15075576
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-02-19 23:50:23 -0500

    Forgot to include the new files in the last commit

---
+++ R/layer_boxplot.R
@@ -0,0 +1,150 @@
+#' Title
+#'
+#' @param sce_layer
+#' @param sig_genes
+#'
+#' @return
+#' @references Adapted from
+#' https://github.com/LieberInstitute/HumanPilot/blob/master/Analysis/Layer_Guesses/layer_specificity.R
+#' @export
+#'
+#' @examples
+#'
+#' ori_modeling_results <- fetch_data(type = 'modeling_results')
+#' ori_sce_layer <- fetch_data(type = 'sce_layer')
+#'
+#' ## Top 2 genes from the specificity model
+#' sig_genes <- sig_genes_extract_all(
+#'     n = 2,
+#'     modeling_results = ori_modeling_results,
+#'     sce_layer = ori_sce_layer
+#' )
+#'
+#' layer_boxplot(sig_genes = sig_genes, sce_layer = ori_sce_layer)
+#' layer_boxplot(sig_genes = sig_genes, short_title = FALSE, sce_layer = ori_sce_layer)
+#'
+#' layer_boxplot(i = which(sig_genes$model_type == 'anova')[1], sig_genes = sig_genes, sce_layer = ori_sce_layer)
+#' layer_boxplot(i = which(sig_genes$model_type == 'pairwise')[1], sig_genes = sig_genes, sce_layer = ori_sce_layer)
+#'
+layer_boxplot <- function(i = 1,
+    sig_genes = sig_genes_extract(),
+    short_title = TRUE,
+    sce_layer = fetch_data(type = 'sce_layer'),
+    col_bkg_box = 'grey80',
+    col_bkg_point = 'grey40',
+    col_low_box = 'violet',
+    col_low_point = 'darkviolet',
+    col_high_box = 'skyblue',
+    col_high_point = 'dodgerblue4',
+    seed = 20200206) {
+    ## Extract the logcounts
+    mat <- assays(sce_layer)$logcounts
+
+    ## Some internal functions
+    add_rest <- function(x) {
+        if (grepl('full|noWM', x))
+            return(x)
+        ifelse(grepl('-', x), x, paste0(x, '-rest'))
+    }
+
+    assign_color <- function(x, bkg, high, low) {
+        if (grepl('-', x)) {
+            col <- rep(bkg, 7)
+            col[levels(sce_layer$layer_guess_reordered) == gsub('-.*', '', x)] <-
+                high
+            col[levels(sce_layer$layer_guess_reordered) == gsub('.*-', '', x)] <-
+                low
+        } else {
+            col <- rep(low, 7)
+            col[levels(sce_layer$layer_guess_reordered) == x] <-
+                high
+            if (grepl('noWM', x))
+                col[levels(sce_layer$layer_guess_reordered) == 'WM'] <- bkg
+        }
+        names(col) <- levels(sce_layer$layer_guess_reordered_short)
+        return(col)
+    }
+
+    add_bkg_col <- function(x) {
+        assign_color(x,
+            bkg = col_bkg_box,
+            high = col_high_box,
+            low = col_low_box)
+    }
+
+    add_col <- function(x) {
+        assign_color(x,
+            bkg = col_bkg_point,
+            high = col_high_point,
+            low = col_low_point)
+    }
+
+    title <- if (short_title) {
+        paste(sig_genes$gene[i],
+            gsub('-', '>', add_rest(
+                gsub('ayer', '', sig_genes$test[i])
+            )),
+            paste0('p=', formatC(
+                sig_genes$pval[i],
+                format = "e",
+                digits = 2
+            )))
+    } else {
+        paste(
+            sig_genes$gene[i],
+            sig_genes$ensembl[i],
+            sig_genes$layer[i],
+            '\n',
+
+            'tstat',
+            formatC(
+                sig_genes$tstat[i],
+                format = "e",
+                digits = 2
+            ),
+            'p',
+            formatC(
+                sig_genes$pval[i],
+                format = "e",
+                digits = 2
+            ),
+
+            '\n',
+            gsub('top', 'r', gsub(
+                'Layer',
+                'L',
+                sapply(sig_genes$results[i], paste0, collapse = ';')
+            ))
+        )
+    }
+
+    set.seed(seed)
+    if (short_title) {
+        par(mar = c(2, 5, 2, 1) + 0.1)
+    } else {
+        par(mar = c(2, 5, 6, 1) + 0.1)
+    }
+
+    # message(paste(Sys.time(), 'making the plot for', i, 'gene', sig_genes$gene[i]))
+    boxplot(
+        mat[sig_genes$gene_index[i], ] ~ sce_layer$layer_guess_reordered_short,
+        xlab = '',
+        ylab = 'logcounts',
+        main = title,
+        outline = FALSE,
+        cex = 2,
+        cex.axis = 2,
+        cex.lab = 2,
+        cex.main = ifelse(short_title, 2, 1.5),
+        col = add_bkg_col(sig_genes$test[i])
+    )
+    points(
+        mat[sig_genes$gene_index[i], ] ~ jitter(as.integer(
+            sce_layer$layer_guess_reordered_short
+        )),
+        pch = 21,
+        # bg = Polychrome::palette36.colors(7)[as.integer(sce_layer$layer_guess)],
+        bg = add_col(sig_genes$test[i])[as.character(sce_layer$layer_guess_reordered_short)],
+        cex = 2
+    )
+}
---
+++ R/sig_genes_extract.R
@@ -0,0 +1,98 @@
+#' Title
+#'
+#' @param n
+#' @param modeling_results
+#' @param model_type
+#' @param reverse
+#' @param sce_layer
+#'
+#' @return
+#' @references Adapted from
+#' https://github.com/LieberInstitute/HumanPilot/blob/master/Analysis/Layer_Guesses/layer_specificity_functions.R
+#' @export
+#' @importFrom rafalib splitit
+#'
+#' @examples
+#'
+#' ori_modeling_results <- fetch_data(type = 'modeling_results')
+#' ori_sce_layer <- fetch_data(type = 'sce_layer')
+#'
+#' ## anova top 10 genes
+#' sig_genes_extract(
+#'     modeling_results = ori_modeling_results,
+#'     sce_layer = ori_sce_layer
+#' )
+#'
+#' sig_genes_extract(
+#'     modeling_results = ori_modeling_results,
+#'     sce_layer = ori_sce_layer,
+#'     n = nrow(ori_sce_layer)
+#' )
+#'
+sig_genes_extract <- function(
+    n = 10,
+    modeling_results = fetch_data(type = 'modeling_results'),
+    model_type = names(modeling_results)[1],
+    reverse = FALSE,
+    sce_layer = fetch_data(type = 'sce_layer')) {
+
+    model_results <- modeling_results[[model_type]]
+
+    tstats <-
+        model_results[, grep('[f|t]_stat_', colnames(model_results))]
+    colnames(tstats) <- gsub('[f|t]_stat_', '', colnames(tstats))
+
+    if(reverse) {
+        tstats <- tstats * -1
+        colnames(tstats) <- sapply(strsplit(colnames(tstats), '-'), function(x) paste(rev(x), collapse = '-'))
+    }
+
+    pvals <-
+        model_results[, grep('p_value_', colnames(model_results))]
+    fdrs <- model_results[, grep('fdr_', colnames(model_results))]
+
+    sig_genes <- apply(tstats, 2, function(x) {
+        rowData(sce_layer)$gene_name[order(x, decreasing = TRUE)[seq_len(n)]]
+    })
+
+    sig_i <- apply(tstats, 2, function(x) {
+        order(x, decreasing = TRUE)[seq_len(n)]
+    })
+    sig_genes_tstats <-
+        sapply(seq_len(ncol(sig_i)), function(i) {
+            tstats[sig_i[, i], i]
+        })
+    sig_genes_pvals <-
+        sapply(seq_len(ncol(sig_i)), function(i) {
+            pvals[sig_i[, i], i]
+        })
+    sig_genes_fdr <-
+        sapply(seq_len(ncol(sig_i)), function(i) {
+            fdrs[sig_i[, i], i]
+        })
+    dimnames(sig_genes_fdr) <-
+        dimnames(sig_genes_tstats) <-
+        dimnames(sig_genes_pvals) <- dimnames(sig_genes)
+
+    ## Combine into a long format table
+    sig_genes_tab <- data.frame(
+        top = rep(seq_len(n), n = ncol(tstats)),
+        model_type = model_type,
+        test = rep(colnames(sig_genes), each = n),
+        gene = as.character(sig_genes),
+        tstat = as.numeric(sig_genes_tstats),
+        pval = as.numeric(sig_genes_pvals),
+        fdr = as.numeric(sig_genes_fdr),
+        gene_index = as.integer(sig_i),
+        stringsAsFactors = FALSE
+    )
+    sig_genes_tab$ensembl <-
+        rownames(sce_layer)[sig_genes_tab$gene_index]
+
+    ## Add gene marker labels
+    # sig_genes_tab <-
+    #     cbind(sig_genes_tab, gene_ann(sig_genes_tab$gene))
+    rownames(sig_genes_tab) <- NULL
+
+    return(sig_genes_tab)
+}
