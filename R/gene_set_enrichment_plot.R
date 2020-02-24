120d7c88787fc55749c11dc8ab7d666d32cd3f2c
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-02-24 15:38:51 -0500

    Drop lattice as a dependency. Created layer_matrix_plot() which is used by layer_cor_plot() and gene_set_enrichment_plot() now
    Former-commit-id: 81a6ef6f4eca7124f5bba30df9031a5cb4ee67db
    Former-commit-id: 8f7cdf263b5ffe1233aa5b54480306b10523a190

--- R/gene_set_enrichment_plot.R
+++ R/gene_set_enrichment_plot.R
@@ -5,7 +5,7 @@
 #'
 #' @param enrichment The output of [gene_set_enrichment()].
 #' @param xlabs A vector of names in the same order and length as
-#' `unique(enrichment$ID)`.
+#' `unique(enrichment$ID)`. Gets passed to [layer_matrix_plot()].
 #' @param PThresh A `numeric(1)` specifying the P-value threshold for the
 #' maximum value in the `-log10(p)` scale.
 #' @param ORcut A `numeric(1)` specifying the P-value threshold for the
@@ -16,17 +16,17 @@
 #' @param layerHeights A `numeric()` vector of length equal to
 #' `length(unique(enrichment$test)) + 1` that starts at 0 specifying where
 #' to plot the y-axis breaks which can be used for re-creating the length of
-#' each brain layer.
-#' @param mypal A vector with the color palette to use.
+#' each brain layer. Gets passed to [layer_matrix_plot()].
+#' @param mypal A vector with the color palette to use. Gets passed to
+#' [layer_matrix_plot()].
 #'
 #' @return A plot visualizing the gene set enrichment
 #' odds ratio and p-value results.
 #' @export
-#' @importFrom fields image.plot
-#' @importFrom graphics axis text abline
 #' @importFrom stats reshape
 #' @family Gene set enrichment functions
 #' @author Andrew E Jaffe, Leonardo Collado-Torres
+#' @seealso layer_matrix_plot
 #' @details Check
 #' https://github.com/LieberInstitute/HumanPilot/blob/master/Analysis/Layer_Guesses/check_clinical_gene_sets.R
 #' to see a full script from where this family of functions is derived from.
@@ -62,9 +62,15 @@
 #' )
 #'
 #' ## Visualize the gene set enrichment results
+#' ## with a custom color palette
 #' gene_set_enrichment_plot(
 #'     asd_sfari_enrichment,
-#'     xlabs = gsub('.*_', '', unique(asd_sfari_enrichment$ID))
+#'     xlabs = gsub('.*_', '', unique(asd_sfari_enrichment$ID)),
+#'     mypal = c("white",
+#'            grDevices::colorRampPalette(
+#'                RColorBrewer::brewer.pal(9, "BuGn")
+#'            )(50)
+#'     )
 #' )
 #'
 #' ## Specify the layer heights so it resembles more the length of each
@@ -72,7 +78,7 @@
 #' gene_set_enrichment_plot(
 #'     asd_sfari_enrichment,
 #'     xlabs = gsub('.*_', '', unique(asd_sfari_enrichment$ID)),
-#'     layerHeights = c(0, 40, 55, 75, 85, 110, 120, 135)
+#'     layerHeights = c(0, 40, 55, 75, 85, 110, 120, 135),
 #' )
 #'
 
@@ -84,8 +90,7 @@ gene_set_enrichment_plot <-
         enrichOnly = FALSE,
         layerHeights = c(0, seq_len(length(unique(enrichment$test)))) * 15,
         mypal = c("white",
-            grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "BuGn"))(50))) {
-
+            grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "YlOrRd"))(50))) {
         ## Re-order and shorten names if they match our data
         if (all(unique(enrichment$test) %in% c('WM', paste0('Layer', seq_len(6))))) {
             enrichment$test <-
@@ -102,7 +107,8 @@ gene_set_enrichment_plot <-
         stopifnot(length(xlabs) == length(unique(enrichment$ID)))
 
         ## Convert to -log10 scale and threshold the pvalues
-        enrichment$log10_P_thresh <- round(-log10(enrichment$Pval), 2)
+        enrichment$log10_P_thresh <-
+            round(-log10(enrichment$Pval), 2)
         enrichment$log10_P_thresh[which(enrichment$log10_P_thresh > PThresh)] <-
             PThresh
 
@@ -123,7 +129,8 @@ gene_set_enrichment_plot <-
                     drop = colnames(enrichment)[!colnames(enrichment) %in% c('ID', 'test', var)],
                     sep = '_mypattern_'
                 )[, -1, drop = FALSE]
-            colnames(res) <- gsub('.*_mypattern_', '', colnames(res))
+            colnames(res) <-
+                gsub('.*_mypattern_', '', colnames(res))
             rownames(res) <- unique(enrichment$ID)
             res <- res[, levels(as.factor(enrichment$test))]
             t(res)
@@ -131,41 +138,11 @@ gene_set_enrichment_plot <-
         wide_or <- make_wide('OR_char')
         wide_p <- make_wide('log10_P_thresh')
 
-        ## For the y-axis labels
-        midpoint <- function(x)
-            x[-length(x)] + diff(x) / 2
-
-        ## Make the plot
-        fields::image.plot(
-            x = seq(0, ncol(wide_p), by = 1),
-            y = layerHeights,
-            z = as.matrix(t(wide_p)),
-            col = mypal,
-            xaxt = "n",
-            yaxt = "n",
-            xlab = "",
-            ylab = ""
-        )
-        axis(2,
-            rownames(wide_or),
-            at = midpoint(layerHeights),
-            las = 1)
-        axis(1, rep("", ncol(wide_p)), at = seq(0.5, ncol(wide_p) - 0.5))
-        text(
-            x = seq(0.5, ncol(wide_p) - 0.5),
-            y = -1 * max(nchar(xlabs)) / 2,
-            xlabs,
-            xpd = TRUE,
-            srt = 45,
-            cex = 2,
-            adj = 1
-        )
-        abline(h = layerHeights, v = c(0, seq_len(ncol(wide_p))))
-        text(
-            x = rep(seq(0.5, ncol(wide_p) - 0.5), each = nrow(wide_p)),
-            y = rep(midpoint(layerHeights), ncol(wide_p)),
-            as.character(wide_or),
-            cex = 1.5,
-            font = 2
+        layer_matrix_plot(
+            matrix_values = wide_p,
+            matrix_labels = wide_or,
+            xlabs = xlabs,
+            layerHeights = layerHeights,
+            mypal = mypal
         )
     }
