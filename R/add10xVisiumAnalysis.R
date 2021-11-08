4a4c2b28d61c54d944936333c7f80253b24cefbf
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-11-08 13:28:16 -0500

    Trying to cherry-pick

---
+++ R/add10xVisiumAnalysis.R
@@ -0,0 +1,72 @@
+#' Add analysis data from a 10x Genomics Visium experiment to a SPE object
+#'
+#' This function adds to a SPE
+#' ([SpatialExperiment-class][SpatialExperiment::SpatialExperiment-class])
+#' object the output from `read10xVisiumAnalysis()`.
+#'
+#' You might want to use `read10xVisiumWrapper()` instead of using this
+#' function directly.
+#'
+#' @param spe A
+#' [SpatialExperiment-class][SpatialExperiment::SpatialExperiment-class] object.
+#' @param visium_analysis The output from `read10xVisiumAnalysis()`.
+#'
+#' @return A
+#' [SpatialExperiment-class][SpatialExperiment::SpatialExperiment-class] object
+#' with the clustering results from SpaceRanger added to `colData(spe)` and
+#' the dimension reduction results added to `reducedDims(spe)`. Added data
+#' starts with the `10x_` prefix to make them easy to differentiate.
+#' @export
+#' @family Utility functions for reading data from SpaceRanger output by 10x
+#' Genomics
+#'
+#' @examples
+#' ## See 'Using spatialLIBD with 10x Genomics public datasets' for
+#' ## a full example using this function.
+#' if (interactive()) {
+#'     browseVignettes(package = "spatialLIBD")
+#' }
+#'
+#' ## Note that ?SpatialExperiment::read10xVisium doesn't include all the files
+#' ## we need to illustrate read10xVisiumWrapper().
+add10xVisiumAnalysis <- function(spe,
+    visium_analysis) {
+    col_info <- colData(spe)
+    barcode_present <- "barcode" %in% colnames(col_info)
+    if (!barcode_present) {
+        col_info$barcode <- rownames(col_info)
+    }
+
+    merged_info <-
+        merge(
+            col_info,
+            visium_analysis$clusters,
+            by = c("barcode", "sample_id"),
+            sort = FALSE,
+            all = TRUE
+        )
+
+    key_spe <- paste0(col_info$barcode, "_", col_info$sample_id)
+    key_analysis <- paste0(merged_info$barcode, "_", merged_info$sample_id)
+    m <- match(key_spe, key_analysis)
+    merged_info <- merged_info[m, ]
+    rownames(merged_info) <- rownames(col_info)
+
+    if (!barcode_present) {
+        merged_info$barcode <- NULL
+    }
+
+    basic_structure <- col_info[, c("barcode", "sample_id")]
+    projections_list <- lapply(visium_analysis$projections, function(x) {
+        merged_projection <- merge(basic_structure, x, sort = FALSE, all = TRUE)
+        key_projection <- paste0(merged_projection$barcode, "_", merged_projection$sample_id)
+        m_proj <- match(key_spe, key_projection)
+        result <- merged_projection[m_proj, -which(colnames(merged_projection) %in% c("barcode", "sample_id"))]
+        rownames(result) <- rownames(col_info)
+        return(as.matrix(result))
+    })
+
+    reducedDims(spe) <- c(reducedDims(spe), projections_list)
+    colData(spe) <- DataFrame(merged_info, check.names = FALSE)
+    return(spe)
+}
---
+++ R/read10xVisiumAnalysis.R
@@ -0,0 +1,108 @@
+#' Load analysis data from a 10x Genomics Visium experiment
+#'
+#' This function expands [SpatialExperiment::read10xVisium()] by reading
+#' analysis outputs from SpaceRanger by 10x Genomics.
+#'
+#' You might want to use `read10xVisiumWrapper()` instead of using this
+#' function directly.
+#'
+#' @inheritParams read10xVisiumWrapper
+#'
+#' @return A named `list()` with the information about the clustering and the
+#' dimension reduction (projections) from the SpaceRanger output by 10x
+#' Genomics.
+#' @export
+#' @family Utility functions for reading data from SpaceRanger output by 10x
+#' Genomics
+#'
+#' @examples
+#' ## See 'Using spatialLIBD with 10x Genomics public datasets' for
+#' ## a full example using this function.
+#' if (interactive()) {
+#'     browseVignettes(package = "spatialLIBD")
+#' }
+#'
+#' ## Note that ?SpatialExperiment::read10xVisium doesn't include all the files
+#' ## we need to illustrate read10xVisiumWrapper().
+read10xVisiumAnalysis <- function(samples = "",
+    sample_id = paste0("sample", sprintf("%02d", seq_along(samples)))) {
+
+    # check sample identifiers
+    if (is.null(sids <- names(samples))) {
+        if (is.null(sids <- sample_id)) {
+            stop("'sample_id' mustn't be NULL when 'samples' are unnamed")
+        } else if (!is.character(sample_id) &&
+            length(unique(sample_id)) != length(samples)) {
+              stop("'sample_id' should contain as many unique values as 'samples'")
+          }
+    } else if (length(unique(sids)) != length(samples)) {
+          stop("names of 'samples' should be unique")
+      }
+    names(samples) <- sids
+
+    dir <- file.path(samples, "analysis")
+
+    current_dir <- dir[1]
+    current_sample <- sids[1]
+
+    clusters_all <- do.call(rbind, mapply(function(current_dir, current_sample) {
+        clustering_files <-
+            list.files(
+                current_dir,
+                pattern = "clusters.csv",
+                all.files = TRUE,
+                full.names = TRUE,
+                recursive = TRUE
+            )
+
+        clusters_list <- lapply(clustering_files, read_barcoded_csv)
+        clusters <- Reduce(function(...) merge(..., by = "barcode", all = TRUE), clusters_list)
+        clusters$sample_id <- current_sample
+        return(clusters)
+    }, dir, sids, SIMPLIFY = FALSE, USE.NAMES = FALSE))
+
+
+    projection_all <- mapply(function(current_dir, current_sample) {
+        projection_files <-
+            list.files(
+                current_dir,
+                pattern = "projection.csv",
+                all.files = TRUE,
+                full.names = TRUE,
+                recursive = TRUE
+            )
+
+        projection_list <- lapply(projection_files, function(x) {
+            res <- read_barcoded_csv(x)
+            res$sample_id <- current_sample
+            return(res)
+        })
+        names(projection_list) <- paste0("10x_", basename(dirname(dirname(projection_files))))
+
+        return(projection_list)
+    }, dir, sids, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+
+    projection_names <- unique(unlist(lapply(projection_all, names)))
+    projections_combined <- lapply(projection_names, function(projection_name) {
+        one_projection_list <- lapply(projection_all, "[[", projection_name)
+        do.call(rbind, one_projection_list)
+    })
+    names(projections_combined) <- projection_names
+
+
+    cluster_cols <- which(!colnames(clusters_all) %in% c("barcode", "sample_id"))
+    colnames(clusters_all)[cluster_cols] <- paste0("10x_", colnames(clusters_all)[cluster_cols])
+
+    return(list(clusters = clusters_all, projections = projections_combined))
+}
+
+
+read_barcoded_csv <- function(x) {
+    df <- read.csv(x)
+    colnames(df) <- tolower(colnames(df))
+
+    if (colnames(df)[2] == "cluster") {
+        colnames(df)[2] <- basename(dirname(x))
+    }
+    return(df)
+}
