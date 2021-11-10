7d739f7ee2c07ec49b3b2db32ba266051a816ee7
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-11-10 16:37:52 -0500

    Started documenting add_image() then realized that SpatialExperiment::addImg() is very similar

--- R/add_image.R
+++ R/add_image.R
@@ -1,14 +1,55 @@
+#' Title
+#'
+#' @param spe
+#' @param image_path
+#' @param sample_id A `character(1)`
+#' @param image_id A `character(1)` specifying the ID to use for the new image.
+#' @param scale_factor A `numeric(1)` with the scaling factor. If you are using
+#' an image that has the same dimensions as an existing one, you could use
+#' [SpatialExperiment::scaleFactors][SpatialExperiment::SpatialExperiment-methods].
+#' @param overwrite
+#'
+#' @return
+#' @importFrom SpatialExperiment imgData "imgData<-" scaleFactors
+#' @export
+#'
+#' @examples
+#' if (enough_ram()) {
+#'     ## Obtain the necessary data
+#'     if (!exists("spe")) spe <- fetch_data("spe")
+#'
+#'     ## Add an image
+#'     SpatialExperiment::imgData(add_image(
+#'         spe = spe,
+#'         image_path = "https://spatial-dlpfc.s3.us-east-2.amazonaws.com/images/151507_tissue_hires_image.png",
+#'         sample_id = "151507",
+#'         image_id = "hires",
+#'         scale_factor = 0.150015
+#'     ))
+#'
+#'     SpatialExperiment::imgData(addImg(
+#'         spe,
+#'         imageSource = "https://spatial-dlpfc.s3.us-east-2.amazonaws.com/images/151507_tissue_hires_image.png",
+#'         sample_id = "151507",
+#'         image_id = "hires",
+#'         scaleFactor = 0.150015
+#'     ))
+#'
+#'     ## scale factor information from
+#'     ## https://github.com/LieberInstitute/HumanPilot/blob/master/10X/151507/scalefactors_json.json
+#' }
 add_image <-
     function(spe,
         image_path,
         sample_id,
-        scale_factor,
         image_id = gsub("\\..*", "", basename(image_path)),
+        scale_factor = SpatialExperiment::scaleFactors(spe, sample_id = sample_id, image_id = image_id),
         overwrite = FALSE) {
+        img_data <- SpatialExperiment::imgData(spe)
+        img_exist_i <- img_data$image_id == image_id & img_data$sample_id == sample_id
+
         if (!overwrite) {
-            if (length(
-                SpatialExperiment::scaleFactors(spe, sample_id = sample_id, image_id = image_id)
-            ) > 0) {
+            if (any(img_exist_i)) {
                 warning("Did you mean to overwrite image_id '",
                     image_id,
                     "'?",
@@ -18,8 +59,7 @@ add_image <-
         } else {
             ## Remove the input image to avoid duplicating it later on
             ## when we rbind()
-            img_data <- SpatialExperiment::imgData(spe)
-            img_data <- img_data[ !(img_data$image_id == image_id & img_data$sample_id == sample_id),  ]
+            img_data <- img_data[ !(img_exist_i),  ]
         }
 
         new_image_data <-
