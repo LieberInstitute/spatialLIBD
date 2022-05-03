1a992f9800d65d6cb1435f82324ebe857a8a3ebb
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-05-02 22:34:27 -0400

    Fix bug initializing the app UI when run_app(sce_layer = NULL)
    Reported by @heenadivecha and @Anthony-Ramnauth

--- R/app_ui.R
+++ R/app_ui.R
@@ -15,12 +15,6 @@ app_ui <- function() {
     modeling_results <- golem::get_golem_options("modeling_results")
     sig_genes <- golem::get_golem_options("sig_genes")
 
-    default_model_type <- ifelse(
-        "enrichment" %in% names(modeling_results),
-        "enrichment",
-        names(modeling_results)[1]
-    )
-
     red_dim_names <- reducedDimNames(spe)
     if (length(red_dim_names) > 0) {
         red_dim_names <- sort(red_dim_names)
@@ -28,11 +22,20 @@ app_ui <- function() {
         red_dim_names <- NULL
     }
 
-    default_reduced_dim <- ifelse(
-        "PCA" %in% reducedDimNames(sce_layer),
-        "PCA",
-        reducedDimNames(sce_layer)[1]
-    )
+    if (!is.null(sce_layer)) {
+        default_model_type <- ifelse(
+            "enrichment" %in% names(modeling_results),
+            "enrichment",
+            names(modeling_results)[1]
+        )
+
+        default_reduced_dim <- ifelse(
+            "PCA" %in% reducedDimNames(sce_layer),
+            "PCA",
+            reducedDimNames(sce_layer)[1]
+        )
+    }
+
 
     tagList(
         # Leave this function for adding external resources
