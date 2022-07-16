4c8933bd5d962cdb7cf8d5f1127d35c5146e5854
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-07-16 04:39:51 -0400

    load markdown for shinyapps.io

--- app.R
+++ app.R
@@ -1,3 +1,5 @@
+library("markdown") ## due to a shinyapps.io bug
+
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
