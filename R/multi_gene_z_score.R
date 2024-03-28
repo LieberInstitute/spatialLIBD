d38e1e0f668a1177062e6efeaeeee5d0f2872abc
Nick-Eagles <nick.eagles@libd.org>
2024-03-28 10:47:29 -0400

    Make the z-score multi-gene method more robust against NA values, and put the checks inside 'multi_gene_z_score' instead of 'vis_gene' to make things more robust to direct calls to 'multi_gene_z_score' (as done in the Shiny app, for example)

--- R/multi_gene_z_score.R
+++ R/multi_gene_z_score.R
@@ -14,9 +14,26 @@
 #' @family functions for summarizing expression of multiple continuous variables simultaneously
 #' @keywords internal
 multi_gene_z_score <- function(cont_mat) {
+    #   Z-score calculation requires at least 2 features with nonzero variance.
+    #   Verify this and drop any zero-variance features
+    good_indices <- which(colSds(cont_mat, na.rm = TRUE) != 0)
+    if (length(good_indices) < 2) {
+        stop("After dropping features with no expression variation, less than 2 features were left")
+    }
+    if (ncol(cont_mat) - length(good_indices) > 0) {
+        warning(
+            sprintf(
+                "Dropping features(s) '%s' which have no expression variation",
+                paste(colnames(cont_mat)[-good_indices], collapse = "', '")
+            )
+        )
+    }
+    cont_mat = cont_mat[, good_indices]
+
     #   For each spot, average Z-scores across all features
-    cont_z <- (cont_mat - colMeans(cont_mat)) / colSds(cont_mat)
-    z_vec <- rowMeans(cont_z, na.rm = TRUE)
+    cont_z <- (cont_mat - colMeans(cont_mat, na.rm = TRUE)) /
+        colSds(cont_mat, na.rm = TRUE)
+    z_vec <- rowMeans(cont_z)
 
     return(z_vec)
 }
