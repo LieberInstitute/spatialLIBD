c81f0ba1804092f2bf164fecb0ad5d47944f8eb0
Louise Huuki <lahuuki@gmail.com>
2024-05-06 13:18:18 -0400

    Add more examples, use tidy style tools to find edge spots + edge distance

--- R/metrics_qc.R
+++ R/metrics_qc.R
@@ -12,17 +12,25 @@
 #'     if (!exists("spe")) spe <- fetch_data("spe")
 #'     
 #'     
-#'     vis_gene(
-#'         spe = spe,
-#'         gene= "sum_umi",
-#'         sampleid = "151507",
-#'         cont_colors = rev(viridisLite::viridis(21, option = "magma"))
-#'     )
-#'     
+#'     ## adds QC metrics to colData of the spe
 #'     spe_qc <- metrics_qc(spe)
+#'     colData(spe_qc)
 #'     
-#'     ## visulize edge spots
-#'     vis_clus(spe_qc, sample_id = "151507", clustervar = "edge_spots")
+#'     table(spe_qc$edge_spot, spe_qc$scran_low_lib_size_edge)
+#'     
+#'     ## visualize edge spots
+#'     vis_clus(spe_qc, sample_id = "151507", clustervar = "edge_spot")
+#'     vis_gene(spe_qc, sample_id = "151507", geneid = "edge_distance", minCount = -1)
+#'     
+#'     ## visualize scran QC flags
+#'     
+#'     vis_clus(spe_qc, sample_id = "151507", clustervar = "scran_low_lib_size")
+#'     
+#'     scater::plotColData(spe_qc, x = "sample_id", y = "sum_umi", colour_by = "scran_low_lib_size")
+#'     
+#'     vis_clus(spe_qc, sample_id = "151507", clustervar = "scran_low_n_features")
+#'     vis_clus(spe_qc, sample_id = "151507", clustervar = "scran_discard")
+#'     vis_clus(spe_qc, sample_id = "151507", clustervar = "scran_low_lib_size_edge")
 #'     
 #'     }
 #'     #'     
@@ -36,17 +44,14 @@ metrics_qc <- function(spe) {
     sample_id = spe$sample_id
   )
   
-  qcfilter <- DataFrame(
+  qcfilter <- data.frame(
     low_lib_size = scater::isOutlier(qc_df$log2sum, type = "lower", log = TRUE, batch = qc_df$sample_id),
     low_n_features = scater::isOutlier(qc_df$log2detected, type = "lower", log = TRUE, batch = qc_df$sample_id),
     high_subsets_Mito_percent = scater::isOutlier(qc_df$subsets_Mito_percent, type = "higher", batch = qc_df$sample_id)
-  )
-  qcfilter$discard <- (qcfilter$low_lib_size | qcfilter$low_n_features) | qcfilter$high_subsets_Mito_percent
+  ) |> mutate(discard  = (low_lib_size | low_n_features) | high_subsets_Mito_percent)
   
   
-  spe$scran_low_lib_size_low_mito <- factor(qcfilter$low_lib_size & qc_df$subsets_Mito_percent < 0.5, levels = c("TRUE", "FALSE"))
-  
-  
+  ## Add qcfilter cols to colData(spe) after factoring
   spe$scran_discard <-
     factor(qcfilter$discard, levels = c("TRUE", "FALSE"))
   
@@ -59,34 +64,57 @@ metrics_qc <- function(spe) {
   spe$scran_high_subsets_Mito_percent <-
     factor(qcfilter$high_subsets_Mito_percent, levels = c("TRUE", "FALSE"))
   
+  spe$scran_low_lib_size_low_mito <- 
+    factor(qcfilter$low_lib_size & 
+             qc_df$subsets_Mito_percent < 0.5, levels = c("TRUE", "FALSE"))
+  
   ## Find edge spots
-  spots <- data.frame(
-    row = spe$array_row,
-    col = spe$array_col,
-    sample_id = spe$sample_id
-  )
   
-  edge_spots_row <- group_by(spots, sample_id, row) |> dplyr::summarize(min_col = min(col), max_col = max(col))
-  edge_spots_col <- group_by(spots, sample_id, col) |> dplyr::summarize(min_row = min(row), max_row = max(row))
+  ## Find edge spots
+  spot_coords <- colData(spe) |>
+    as.data.frame() |>
+    select(sample_id, array_row, array_col) |>
+    group_by(sample_id, array_row) |>
+    mutate(edge_col = array_col == min(array_col) | array_col == max(array_col),
+           col_distance = pmin(abs(array_col - min(array_col)),
+                               abs(array_col - max(array_col)))
+           ) |>
+    group_by(sample_id, array_col) |>
+    mutate(edge_row = array_row == min(array_row) | array_row == max(array_row),
+           row_distance = pmin(abs(array_row - min(array_row)),
+                               abs(array_row - max(array_row)))
+           ) |>
+    group_by(sample_id) |>
+    mutate(edge_spot = edge_row | edge_col,
+           edge_distance = pmin(row_distance, col_distance))
   
-  spots <- dplyr::left_join(spots, edge_spots_row) |> dplyr::left_join(edge_spots_col)
-  spots$edge_spots <- with(spots, row == min_row | row == max_row | col == min_col | col == max_col)
+  # spots <- data.frame(
+  #   row = spe$array_row,
+  #   col = spe$array_col,
+  #   sample_id = spe$sample_id
+  # )
   
-  spots$row_distance <- with(spots, pmin(abs(row - min_row), abs(row - max_row)))
-  spots$col_distance <- with(spots, pmin(abs(col - min_col), abs(col - max_col)))
+  # edge_spots_row <- group_by(spots, sample_id, row) |> dplyr::summarize(min_col = min(col), max_col = max(col))
+  # edge_spots_col <- group_by(spots, sample_id, col) |> dplyr::summarize(min_row = min(row), max_row = max(row))
+  # 
+  # spots <- dplyr::left_join(spots, edge_spots_row) |> dplyr::left_join(edge_spots_col)
+  # spots$edge_spots <- with(spots, row == min_row | row == max_row | col == min_col | col == max_col)
+  # 
+  # spots$row_distance <- with(spots, pmin(abs(row - min_row), abs(row - max_row)))
+  # spots$col_distance <- with(spots, pmin(abs(col - min_col), abs(col - max_col)))
   ## spots$edge_distance <- with(spots, sqrt(row_distance^2 + col_distance^2))
   ## The above is from:
   ## sqrt((x_1 - x_2)^2 + (y_1 - y_2)^2)
   ## but it was wrong, here's a case the the smallest distance is on the column:
   ## sqrt(0^2 + col_distance^2) = col_distance
-  spots$edge_distance <- with(spots, pmin(row_distance, col_distance))
+  # spots$edge_distance <- with(spots, pmin(row_distance, col_distance))
   
   
-  spe$edge_spots <- factor(spots$edge_spots, levels = c("TRUE", "FALSE"))
-  spe$edge_distance <- spots$edge_distance
+  spe$edge_spot <- factor(spot_coords$edge_spot,levels = c("TRUE", "FALSE"))
+  spe$edge_distance <- spot_coords$edge_distance
   
   
-  spe$scran_low_lib_size_edge <- factor(qcfilter$low_lib_size & spots$edge_distance < 1, levels = c("TRUE", "FALSE"))
+  spe$scran_low_lib_size_edge <- factor(qcfilter$low_lib_size & spot_coords$edge_spot, levels = c("TRUE", "FALSE"))
   
   return(spe)
 }
