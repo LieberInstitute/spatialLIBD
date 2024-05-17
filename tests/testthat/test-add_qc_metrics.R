535ceea33ce5e04872610aef3963f0fc53a1877c
Louise Huuki <lahuuki@gmail.com>
2024-05-17 15:45:35 -0400

    Update add_qc_metrics name in test

--- tests/testthat/test-add_qc_metrics.R
+++ tests/testthat/test-add_qc_metrics.R
@@ -1,10 +1,10 @@
 test_that(
-    "metrics_qc returns modified spe",
+    "add_qc_metrics returns modified spe",
     {
         if (!exists("spe")) spe <- fetch_data("spe")
 
         # run metrics spe
-        spe_qc <- metrics_qc(spe)
+        spe_qc <- add_qc_metrics(spe)
         expect_equal(ncol(spe), ncol(spe_qc)) ## same number of spots
         expect_equal(ncol(colData(spe)) + 8, ncol(colData(spe_qc))) ## add 8 QC cols to colData
     }
