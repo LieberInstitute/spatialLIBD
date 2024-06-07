d23f5b60a126b598f1670c81990294c1ec293af9
Nick-Eagles <nick.eagles@libd.org>
2024-06-07 11:30:26 -0400

    Further check legitimacy of prep_stitched_data() output

--- tests/testthat/test-prep_stitched_data.R
+++ tests/testthat/test-prep_stitched_data.R
@@ -24,6 +24,14 @@ test_that(
             "^spe\\$exclude_overlapping must include some FALSE values to plot$"
         )
 
+        #   Output should be a list with the correct names, and the
+        #   SpatialExperiment should have no excluded spots
+        spe$exclude_overlapping[1:100] = FALSE
+        temp <- prep_stitched_data(spe, point_size = 2, image_id = "lowres")
+        expect_equal(class(temp), 'list')
+        expect_equal(names(temp), c('spe', 'point_size'))
+        expect_equal(all(temp$spe$exclude_overlapping), FALSE)
+
         #   Note bad image_id is not tested, since this function is only used
         #   internally after checks for legitimate image_id are performed
     }
