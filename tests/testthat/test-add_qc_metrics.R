8cc5dd0a34c3ace5e48e1cbe2ae400361bc850ed
lcolladotor <lcolladotor@gmail.com>
2024-07-11 14:12:23 -0400

    also rm an spe from the test for add_qc_metrics once we are done with it

--- tests/testthat/test-add_qc_metrics.R
+++ tests/testthat/test-add_qc_metrics.R
@@ -10,5 +10,6 @@ test_that(
         # [1] "scran_discard"                   "scran_low_lib_size"              "scran_low_n_features"
         # [4] "scran_high_subsets_Mito_percent" "edge_spot"                       "edge_distance"
         # [7] "scran_low_lib_size_edge"
+        rm(spe_qc)
     }
 )
