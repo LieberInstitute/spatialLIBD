4b92cce638194c9d5b532067969cd239fbc6a3e0
lahuuki <lahuuki@gmail.com>
2025-11-25 14:56:43 +0000

    Add ERC 8 type opttions to fetch_data()

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -103,7 +103,15 @@ fetch_data <-
             "Visium_SPG_AD_Visium_wholegenome_modeling_results",
             "visiumStitched_brain_spe",
             "visiumStitched_brain_spaceranger",
-            "visiumStitched_brain_Fiji_out"
+            "visiumStitched_brain_Fiji_out",
+            "LFF_spatial_ERC_SRT",
+            "LFF_spatial_ERC_SRT_pseudobulk",
+            "LFF_spatial_ERC_SRT_modeling_results",
+            "LFF_spatial_ERC_snRNAseq",
+            "LFF_spatial_ERC_snRNAseq_pseudobulk_broad",
+            "LFF_spatial_ERC_snRNAseq_pseudobulk_subcluster",
+            "LFF_spatial_ERC_snRNAseq_modeling_results_broad",
+            "LFF_spatial_ERC_snRNAseq_modeling_results_subcluster",
         ),
         destdir = tempdir(),
         eh = ExperimentHub::ExperimentHub(),
@@ -286,6 +294,78 @@ fetch_data <-
             file_name <- "visiumStitched_brain_fiji_out.zip"
             url <-
                 "https://www.dropbox.com/scl/fi/bevo52e96f2kdwllf8dkk/visiumStitched_brain_fiji_out.zip?rlkey=ptwal8f5zxakzejwd0oqw0lhj&dl=1"
+        } else if (type == "LFF_spatial_ERC_SRT") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "spe_ERC_annotated.zip"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_SRT_pseudobulk") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "spe_pseudobulk-SpD.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_SRT") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "spe_pseudobulk-SpD.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_SRT_modeling_results") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "modeling_results-SpD.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_snRNAseq") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "sce_ERC_subcluster.zip"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_snRNAseq_pseudobulk_broad") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "sce_subcluster_pseudobulk-cell_type_broad.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_snRNAseq_pseudobulk_subcluster") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "sce_subcluster_pseudobulk-cell_type_anno.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_snRNAseq_modeling_results_broad") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "sce_subcluster_pseudobulk-cell_type_broad.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
+        } else if (type == "LFF_spatial_ERC_snRNAseq_modeling_results_subcluster") {
+          tag <- "LFF_spatial_ERC"
+          hub_title <- type
+          
+          ## While EH is not set-up
+          file_name <- "sce_subcluster_pseudobulk-cell_type_anno.rds"
+          url <-
+            "https://www.dropbox.com/TODO"
         }
 
         file_path <- file.path(destdir, file_name)
